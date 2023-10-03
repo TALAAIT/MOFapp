5cbf3c18d0cada58835bd3bc892600ee958ccc75
Mahmoud <mahmoud.alkateb22@gmail.com>
2023-10-03 03:59:25 -0700

    Talla project setup

---
+++ lib/screens/send_archive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SendArchive extends StatefulWidget {
+  const SendArchive({super.key});
+
+  @override
+  State<SendArchive> createState() => _SendArchiveState();
+}
+
+class _SendArchiveState extends State<SendArchive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

12e7d5965a56566b2e3eb7b35258bdfbcbc765e4
Mahmoud <mahmoud.alkateb22@gmail.com>
2023-10-03 04:05:55 -0700

    screens :expanded the screen directory. Each screen or feature gets its own directory.

---
+++ lib/screens/send_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SendFile extends StatefulWidget {
+  const SendFile({super.key});
+
+  @override
+  State<SendFile> createState() => _SendFileState();
+}
+
+class _SendFileState extends State<SendFile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
